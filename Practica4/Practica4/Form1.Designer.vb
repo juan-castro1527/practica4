5b4936618cc7d7bb519e84a8c76ba805a500903c
mandil01 <juan.castro03@iest.edu.mx>
2026-03-13 08:34:13 -0600

    reloj digital 2

--- Practica4/Practica4/Form1.Designer.vb
+++ Practica4/Practica4/Form1.Designer.vb
@@ -23,12 +23,20 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Horas = New System.Windows.Forms.Label()
         Me.Minutos = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Dia = New System.Windows.Forms.Label()
         Me.Fecha = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Horas
@@ -36,8 +44,8 @@ Partial Class Form1
         Me.Horas.AutoSize = True
         Me.Horas.BackColor = System.Drawing.Color.Transparent
         Me.Horas.Font = New System.Drawing.Font("Agency FB", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Horas.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Horas.Location = New System.Drawing.Point(43, 17)
+        Me.Horas.ForeColor = System.Drawing.Color.GreenYellow
+        Me.Horas.Location = New System.Drawing.Point(143, 35)
         Me.Horas.Name = "Horas"
         Me.Horas.Size = New System.Drawing.Size(137, 115)
         Me.Horas.TabIndex = 0
@@ -48,8 +56,8 @@ Partial Class Form1
         Me.Minutos.AutoSize = True
         Me.Minutos.BackColor = System.Drawing.Color.Transparent
         Me.Minutos.Font = New System.Drawing.Font("Agency FB", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Minutos.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Minutos.Location = New System.Drawing.Point(196, 17)
+        Me.Minutos.ForeColor = System.Drawing.Color.GreenYellow
+        Me.Minutos.Location = New System.Drawing.Point(311, 35)
         Me.Minutos.Name = "Minutos"
         Me.Minutos.Size = New System.Drawing.Size(137, 115)
         Me.Minutos.TabIndex = 1
@@ -60,8 +68,8 @@ Partial Class Form1
         Me.Label2.AutoSize = True
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Agency FB", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Label2.Location = New System.Drawing.Point(157, 17)
+        Me.Label2.ForeColor = System.Drawing.Color.GreenYellow
+        Me.Label2.Location = New System.Drawing.Point(255, 35)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(68, 115)
         Me.Label2.TabIndex = 2
@@ -70,34 +78,85 @@ Partial Class Form1
         'Dia
         '
         Me.Dia.AutoSize = True
-        Me.Dia.Font = New System.Drawing.Font("Agency FB", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Dia.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Dia.Location = New System.Drawing.Point(109, 132)
+        Me.Dia.BackColor = System.Drawing.Color.Transparent
+        Me.Dia.Font = New System.Drawing.Font("Agency FB", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Dia.ForeColor = System.Drawing.Color.GreenYellow
+        Me.Dia.Location = New System.Drawing.Point(229, 150)
         Me.Dia.Name = "Dia"
-        Me.Dia.Size = New System.Drawing.Size(163, 77)
+        Me.Dia.Size = New System.Drawing.Size(115, 59)
         Me.Dia.TabIndex = 3
         Me.Dia.Text = "Lunes"
         '
         'Fecha
         '
         Me.Fecha.AutoSize = True
-        Me.Fecha.Font = New System.Drawing.Font("Agency FB", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Fecha.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Fecha.Location = New System.Drawing.Point(12, 220)
+        Me.Fecha.BackColor = System.Drawing.Color.Transparent
+        Me.Fecha.Font = New System.Drawing.Font("Agency FB", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Fecha.ForeColor = System.Drawing.Color.GreenYellow
+        Me.Fecha.Location = New System.Drawing.Point(156, 209)
         Me.Fecha.Name = "Fecha"
-        Me.Fecha.Size = New System.Drawing.Size(375, 59)
+        Me.Fecha.Size = New System.Drawing.Size(277, 42)
         Me.Fecha.TabIndex = 4
         Me.Fecha.Text = "09 de marzo del 2026"
         '
         'Timer1
         '
         '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1009
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Practica4.My.Resources.Resources.config_2
+        Me.PictureBox1.Location = New System.Drawing.Point(478, 62)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(42, 40)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(71, Byte), Integer), CType(CType(74, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(45, 49)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(495, 202)
+        Me.Panel1.TabIndex = 6
+        Me.Panel1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Agency FB", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(124, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(245, 59)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Configuración"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(22, 71)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.SaddleBrown
-        Me.ClientSize = New System.Drawing.Size(398, 288)
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(585, 340)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Fecha)
         Me.Controls.Add(Me.Dia)
         Me.Controls.Add(Me.Horas)
@@ -105,6 +164,9 @@ Partial Class Form1
         Me.Controls.Add(Me.Label2)
         Me.Name = "Form1"
         Me.Text = "Reloj digital"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -116,4 +178,9 @@ Partial Class Form1
     Friend WithEvents Dia As Label
     Friend WithEvents Fecha As Label
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- Practica4/Practica4/Form1.vb
+++ Practica4/Practica4/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Timer1.Start()
+        Timer2.Start()
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -13,5 +14,21 @@
         Dia.Text = day_o.ToString()
         Horas.Text = time_mod(0)
         Minutos.Text = time_mod(1)
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Label2.Text = ":" Then
+            Label2.Text = ""
+        ElseIf Label2.Text = "" Then
+            Label2.Text = ":"
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If Panel1.Visible = False Then
+            Panel1.Visible = True
+        ElseIf Panel1.Visible = True Then
+            Panel1.Visible = False
+        End If
     End Sub
 End Class
